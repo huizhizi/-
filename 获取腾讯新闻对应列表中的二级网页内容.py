4c2045cde483b56270049b36e01b80416fd88c34
huizhizi <wpeng5566@163.com>
2018-08-14 08:25:16 +0800

    Update 获取腾讯新闻对应列表中的二级网页内容.py
    获取二级网页内容时，增加find('div',class_='content-article')，获取内容更加准确

--- 获取腾讯新闻对应列表中的二级网页内容.py
+++ 获取腾讯新闻对应列表中的二级网页内容.py
@@ -1,12 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-
-
-
 import requests
 from bs4 import BeautifulSoup
 import re
@@ -19,9 +10,9 @@ path=r'/Users/weipeng/Desktop/news.txt'
 f=open(path,'w',encoding='utf8')
 f.seek(0)
 
-f.write('2018年8月10日\n')
+f.write('2018年8月13日\n')
 
-news=soup.find_all('a',href=re.compile('http://new.qq.com/omn/20180809'))
+news=soup.find_all('a',href=re.compile('http://new.qq.com/omn/20180813'))
 #通过正则，获取所有新闻的url
 
 for i in news:
@@ -30,15 +21,17 @@ for i in news:
         continue
     else:
         u=i.attrs['href']
-        #print(u)
+        print(u)
         ur=requests.get(u)
         usoup=BeautifulSoup(ur.text,'lxml')
         f.write(txt+'\n')
         f.write('正文如下：\n')
         
-        p=usoup.find_all('p')
+        p=usoup.find('div',class_='content-article').find_all('p')
+        #print(p)
+        #p=usoup.find_all('p')
         for i in p:
             f.write(i.text+'\n')
-            
+        
 f.close()
 print('完成')
